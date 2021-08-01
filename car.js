6bc52bb34878631b38c2f80ab68c86de689ba67e
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-08-02 00:08:34 +0300

    car2

--- car.js
+++ car.js
@@ -1,20 +1,20 @@
 'use strict';
 let name =prompt("what is your name?");
-alert(" Thinking about getting a cat but not sure which breed is right for you? so welcome to our website we can help you! " +name);
+alert("welcome to my websit " +name);
 console.log(name)
 
 let age =Number(prompt("please enter your age"));
 alert(age);
 console.log(age);
 
-let cat =prompt("do you like cats?");
-alert("welcome to my website");
-console.log(cat)
+let car =prompt("do you like cars?");
+alert("welcome to my cars website");
+console.log(car)
 
 
-let chooseCat =prompt("which cat do you want to know about please choose one of these; Abyssinian Cat , American Shorthair Cat , American Curl Cat Breed ?");
-alert(chooseCat);
-console.log(cat);
+let chooseCar =prompt("which car do you want to have choose one of these (ford, kia, range rover) ?");
+alert(chooseCar);
+console.log(chooseCar);
 
  
 
