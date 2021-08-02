2790bb4de10b0248e0ffb9d7b1f67e0d04a7377f
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-08-02 18:21:06 +0300

    edit html

--- car.js
+++ car.js
@@ -12,7 +12,7 @@ alert("welcome to my cars website");
 console.log(car)
 
 
-let chooseCar =prompt("which car do you want to have choose one of these (ford, kia, range rover) ?");
+let chooseCar =prompt("which car do you want to have choose one of these (ford, kia, range rover)?");
 alert(chooseCar);
 console.log(chooseCar);
 
